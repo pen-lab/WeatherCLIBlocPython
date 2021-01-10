c9bd935adf2444ab0f4a16d2e7f1f459cf9c022f
pen-dev <pelipenkovladislav@gmail.com>
2021-01-11 01:27:48 +0300

    Прогноз погоды по команде в терминале

--- weather/cli.py
+++ weather/cli.py
@@ -1,10 +1,29 @@
+import asyncio
+
+from aiohttp import ClientSession
 import typer
+
+from weather.blocs.weather_event import WeatherRequested
+from weather.blocs.weather_bloc import WeatherBloc
+from weather.repositories.weather_api_client import WeatherApiClient
+from weather.repositories.weather_repository import WeatherRepository
+
+
+async def _main(city: str):
+    async with ClientSession() as session:
+        weather_api_client = WeatherApiClient(session)
+        weather_repository: WeatherRepository = WeatherRepository(weather_api_client)
+        bloc: WeatherBloc = WeatherBloc(weather_repository)
+        await bloc._bind_state_subject()
+
+        await bloc.dispatch(WeatherRequested(city))
+        typer.echo(f'weather to {bloc.state.weather.temp}')
 
 
 def weather(
         city: str = typer.Argument(...)
 ):
-    typer.echo(f'weather to {city}')
+    asyncio.run(_main(city))
 
 
 def main():
